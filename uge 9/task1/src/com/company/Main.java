283d7c2aa98fb58c7f78be6bac4c7fc4a3b566f8
thejamiegc <jcallan32@gmail.com>
2022-03-03 15:26:44 +0100

    task1 with file

--- uge 9/task1/src/com/company/Main.java
+++ uge 9/task1/src/com/company/Main.java
@@ -1,40 +1,47 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
-        String text = "Where is the longest word in this sentence";
-        printLongestWord(text);
-        wordsWithLessThenTwoVocals(text);
+     File data = new File("C:\\Users\\jcall\\Desktop\\data.txt");
+
+        Scanner scan = new Scanner(data);
+        Scanner scan2 = new Scanner(data);
+        printLongestWord(scan);
+
+        wordsWithLessThenTwoVocals(scan2);
     }
 
-    static void printLongestWord(String text){
-        String[] arr = text.split(" ");
+    static void printLongestWord(Scanner scan){
         String longestWord = "";
-        for(int i = 0;i< arr.length;i++){
-            if(arr[i].length()>longestWord.length()){
-                longestWord = arr[i];
+        String input;
+        while(scan.hasNext()){
+            input = scan.next();
+            if(input.length()>longestWord.length()){
+                longestWord = input;
             }
         }
         System.out.println(longestWord);
     }
 
-    static void wordsWithLessThenTwoVocals(String text){
-        String[] arr = text.split(" ");
+    static void wordsWithLessThenTwoVocals(Scanner scan){
+        String currentWord;
         int vocals;
-        for (int i=0;i<arr.length;i++){
-            vocals =0;
-            for (int j=0;j<arr[i].length();j++){
-                if(arr[i].charAt(j) == 'a'||arr[i].charAt(j) == 'e'||arr[i].charAt(j) == 'i'|| arr[i].charAt(j) == 'o'||arr[i].charAt(j) == 'u' || arr[i].charAt(j) == 'A'||arr[i].charAt(j) == 'E'||arr[i].charAt(j) == 'I'|| arr[i].charAt(j) == 'O'||arr[i].charAt(j) == 'U'){
+        while (scan.hasNext()){
+            vocals = 0;
+            currentWord = scan.next();
+            for(int i=0;i<currentWord.length();i++){
+                if(currentWord.charAt(i) == 'a'||currentWord.charAt(i) == 'e'||currentWord.charAt(i) == 'i'||currentWord.charAt(i) == 'o'||currentWord.charAt(i) == 'u'||currentWord.charAt(i) == 'y'||currentWord.charAt(i) == 'æ'||currentWord.charAt(i) == 'ø'||currentWord.charAt(i) == 'å'||currentWord.charAt(i) == 'A'||currentWord.charAt(i) == 'E'||currentWord.charAt(i) == 'I'||currentWord.charAt(i) == 'O'||currentWord.charAt(i) == 'U'||currentWord.charAt(i) == 'Y'||currentWord.charAt(i) == 'Æ'||currentWord.charAt(i) == 'Ø'||currentWord.charAt(i) == 'Å'){
                     vocals++;
                 }
-
             }
-            if(vocals<2) {
-                System.out.print(arr[i] + " ");
+            if(vocals<2){
+                System.out.print(currentWord + " ");
             }
         }
     }
