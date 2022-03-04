db3479cb905cb6c4b8148a59a804522fb2386628
thejamiegc <jcallan32@gmail.com>
2022-03-04 11:54:45 +0100

    add try catch task2

--- uge 9/Task2/src/com/company/Main.java
+++ uge 9/Task2/src/com/company/Main.java
@@ -1,14 +1,20 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        File data = new File("src\\com\\company\\data.txt");
+    public static void main(String[] args) {
+        File data = new File("src/com/company/data.txt");
+        try {
         Scanner scan = new Scanner(data);
         printFirstHalfOfEachWord(scan);
+        }catch(FileNotFoundException exception){
+            System.out.println("file not found");
+            exception.getCause();
+        }
     }
     static void printFirstHalfOfEachWord(Scanner scan){
         String word;
